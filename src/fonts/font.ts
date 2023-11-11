ad14e1963797afe3cff8b784877af43212168b8f
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2023-11-11 23:09:50 +0900

    Initial Commit

---
+++ src/fonts/font.ts
@@ -0,0 +1,8 @@
+import { Nanum_Gothic } from "next/font/google";
+
+const nanumGothic = Nanum_Gothic({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+});
+
+export { nanumGothic };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontSize: {
+      t1: [
+        "57px",
+        {
+          fontWeight: "700",
+          lineHeight: "1.4",
+        },
+      ],
+      t2: [
+        "45px",
+        {
+          lineHeight: "1.4",
+        },
+      ],
+      h1: ["36px", "36px"],
+      h2: ["32px", "32px"],
+      h3: ["28px", "28px"],
+      h4: ["24px", "24px"],
+      b1: ["20px", "20px"],
+      b2: ["18px", "18px"],
+      b3: ["15px", "15px"],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
